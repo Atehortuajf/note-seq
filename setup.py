d4cd6d44b0e5e077fa76a2fbfcb0576e7bb43ee3
Juan F. Atehortua <67188088+Atehortuajf@users.noreply.github.com>
2025-03-01 15:26:46 -0500

    Loosen protobuf requirement

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ REQUIRED_PACKAGES = [
     'numpy',
     'pandas >= 0.18.1',
     'pretty_midi >= 0.2.6',
-    'protobuf >= 4.21.2',
+    'protobuf >= 3.20.3',
     'pydub',
     'scipy >= 0.18.1',
 ]
